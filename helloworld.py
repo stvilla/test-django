37a97dfefd7dafca62565ace4652c823606fc07b
luca <luca>
2021-01-22 22:20:01 +0100

    2o incontro

--- helloworld.py
+++ helloworld.py
@@ -1,3 +1,29 @@
-print("ciao")
-print("ciaostefano")
-print("")
+import test
+
+class Car:
+    def __init__(self,places):
+        print("Car")
+        self.p = places
+        self.is_on = False
+    
+    def start(self):
+        self.is_on = True
+    def __str__(self):
+        return "Places = " + str(self.p) + " is_on = " + str(self.is_on)
+
+def test(a):
+    print(a)
+
+#test()
+
+c = Car(4)
+print(c)
+c.start()
+print(c)
+
+test("ciao")
+
+print(__name__)
+
+#my_str = "ciao"
+#print(my_str.capitalize())
